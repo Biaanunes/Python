22b7b0addef74b21dc1e9a4483a55c93b6b5a576
Biaanunes <bianunesdeveras@gmail.com>
2025-09-03 14:01:10 +0100

    Create soma.py

---
+++ soma.py
@@ -0,0 +1,10 @@
+print('Soma de Números')
+
+numeroUm = int(input('Digite um número: '))
+nuemroDois = int(input('Digite outro número: '))
+
+def soma():
+    resultado = numeroUm + nuemroDois
+    print (f'\nO resultado é {resultado}')
+soma()
+#chame a função depois de codala
