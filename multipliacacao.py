ff1cff177b2abdefeb0e928f637a5e1571e17e0b
Biaanunes <bianunesdeveras@gmail.com>
2025-09-08 22:13:17 +0100

    Create multipliacacao.py

---
+++ multipliacacao.py
@@ -0,0 +1,18 @@
+digitado = int(input("Digite um número de 1 a 10, para saber sua tabuada: "))
+
+resultadoA = 1 * (digitado)
+resultadoB = 2 * (digitado)
+resultadoC = 3 * (digitado)
+resultadoD = 4 * (digitado)
+resultadoE = 5 * (digitado)
+resultadoF = 6 * (digitado)
+resultadoG = 7 * (digitado)
+resultadoH = 8 * (digitado)
+resultadoI = 9 * (digitado)
+resultadoJ = 10 * (digitado)
+
+
+def tabuada():
+  print(f"\n{digitado}x1 = {resultadoA}\n {digitado}x2 = {resultadoB}\n {digitado}x3 = {resultadoC}\n {digitado}x4 = {resultadoD}\n {digitado}x5 = {resultadoE}\n {digitado}x6 = {resultadoF}\n {digitado}x7 = {resultadoG}\n {digitado}x8 = {resultadoH}\n {digitado}x9 = {resultadoI}\n {digitado}x10 = {resultadoJ}")
+
+tabuada()
