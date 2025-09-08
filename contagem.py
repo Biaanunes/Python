387380196da3872ea12a1f3d8471b618457476cd
Biaanunes <bianunesdeveras@gmail.com>
2025-09-08 21:47:50 +0100

    Update contagem.py

--- contagem.py
+++ contagem.py
@@ -5,7 +5,15 @@ while dez < 10:
     dez += 1
 print(dez)
 
+##########
+
 numeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
 for umPorUm in numeros:
     print(umPorUm)
+    
+##########
+
+for index in range(10):
+    print(index)
+print("10")
