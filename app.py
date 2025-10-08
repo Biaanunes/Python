4dbf58cf17093d393c50d755c3eff2d3b64fea33
Biaanunes <bianunesdeveras@gmail.com>
2025-10-08 12:16:42 -0300

    Update MAIOR NÚMERO

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ elif n2>n1 and n2>n3:
 else:
     print(f'O maior número é {n3}')
 
+#OU
+primeiro = int(input('Digite um número: '))
+segundo = int(input('Digite outro número: '))
+terceiro = int(input('Digite o último número: '))
+
+def maiorNumero():
+    resposta = max(primeiro, segundo, terceiro)
+    print(f'O maior número é {resposta}')
+maiorNumero()
+
